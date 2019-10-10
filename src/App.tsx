6c1e156d5428fb28ac106f0bc084e91ea009fade
koushisa <>
2019-10-10 20:04:47 +0900

    add home

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "@material-ui/core/Button";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Restaurant, AccessibilityNew, EventNote } from "@material-ui/icons";
+import { Container, Box } from "@material-ui/core/";
+
+const styles = {
+  largeIcon: {
+    width: 300,
+    height: 300
+  }
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" color="secondary">
-          Hi
-        </Button>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <BrowserRouter>
+        <Link to="/resipe">
+          <Box display="flex" justifyContent="center">
+            <Restaurant
+              color="action"
+              style={{ width: "50%", height: 300 }}
+            ></Restaurant>
+          </Box>
+        </Link>
+
+        <Link to="/weight">
+          <AccessibilityNew
+            color="action"
+            style={{ width: "50%", height: 300 }}
+          ></AccessibilityNew>
+        </Link>
+
+        <Link to="/menu">
+          <EventNote
+            color="action"
+            style={{ width: "50%", height: 300 }}
+          ></EventNote>
+        </Link>
+      </BrowserRouter>
+    </Container>
   );
 };
 
